b86a9a5307994c08c45f84300a8f7236d1dd9115
kishan gupta <kkgupta0106@gmail.com>
2025-03-09 22:35:57 +0530

    tailwind

--- src/card/RestaurantMenu.jsx
+++ src/card/RestaurantMenu.jsx
@@ -37,40 +37,56 @@ function RestaurantMenu() {
   }
 
   return (
-    <div className="flex flex-col m-10 ">
-      <div className="border border-black p-8 text-2xl ">
-        <h1>{resInfo?.name}</h1>
+    <div className="flex flex-col m-10">
+      {/* Restaurant Name Section */}
+      <div className="border border-black p-8 text-2xl">
+        <h1>{resInfo?.name || "Restaurant Name Not Available"}</h1>
       </div>
-      <div className="">
+
+      {/* Menu List Section */}
+      <div>
         <div className="flex justify-center text-center text-2xl mt-6">
           <h1>Menu List</h1>
         </div>
-        <div className="">
-          <h1>{menuData?.cards[1]?.card?.card?.title}</h1>
-          {menuData?.cards[1]?.card?.card?.itemCards?.map((res) => (
-            <div key={res?.card?.info?.id} className="menu-card">
-              <div>
-                <h3>{res?.card?.info?.name}</h3>
-                <p>
+
+        <div>
+          <h1>{menuData?.cards[1]?.card?.card?.title || "No Category"}</h1>
+
+          {menuData?.cards[1]?.card?.card?.itemCards?.length > 0 ? (
+            menuData?.cards[1]?.card?.card?.itemCards.map((res) => (
+              <div key={res?.card?.info?.id} className="menu-card">
+                <div>
+                  <h3>{res?.card?.info?.name || "No Name Available"}</h3>
+                  <p>
                   {res?.card?.info?.price / 100 ||
                     res?.card?.info?.defaultPrice / 100}{" "}
-                  Rs
-                </p>
-                <p className="menu-rating">
-                  🌟
-                  {res?.card?.info?.ratings?.aggregatedRating?.rating || 4.8}
-                </p>
-                <p>{res?.card?.info?.description}</p>
+                    Rs
+                  </p>
+                  <p className="menu-rating">
+                    🌟{" "}
+                    {res?.card?.info?.ratings?.aggregatedRating?.rating ||
+                      "No Rating"}
+                  </p>
+                  <p>
+                    {res?.card?.info?.description || "No description available"}
+                  </p>
+                </div>
+                <div>
+                  {res?.card?.info?.imageId ? (
+                    <img
+                      src={`https://media-assets.swiggy.com/swiggy/image/upload/${res?.card?.info?.imageId}`}
+                      alt={res?.card?.info?.name}
+                      className="menu-img"
+                    />
+                  ) : (
+                    <p>No Image Available</p>
+                  )}
+                </div>
               </div>
-              <div>
-                <img
-                  src={`https://media-assets.swiggy.com/swiggy/image/upload/${res?.card?.info?.imageId}`}
-                  alt="food"
-                  className="menu-img"
-                />
-              </div>
-            </div>
-          ))}{" "}
+            ))
+          ) : (
+            <p>No menu items available</p>
+          )}
         </div>
       </div>
     </div>
